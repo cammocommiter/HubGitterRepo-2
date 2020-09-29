5001487f0cc8e7baf0b2cb82957000c482916cc5
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-09-29 11:58:18 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,15 +1,8 @@
-# Cross-platform way of finding an executable in the $PATH.
-#
-#   which('ruby') #=> /usr/bin/ruby
-def which(cmd)
-  exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
-  ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
-    exts.each do |ext|
-      exe = File.join(path, "#{cmd}#{ext}")
-      return exe if File.executable?(exe) && !File.directory?(exe)
-    end
-  end
-  nil
-end
+p()
+all : usera userb userc....
 
-Kernel#gets
+usera:
+       imapsync usera
+userb:
+       imapsync userb
+....
