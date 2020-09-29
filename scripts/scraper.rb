9c394f554312ded07f68e37d7b21ee7f9d98f3e8
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-09-29 11:57:52 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,1 +1,6 @@
-def this; end
+add-on
+Promotion.joins(:category).
+         where(["lft>=? and rgt<=?", c.lft, c.rgt]).
+         joins(:shops).
+         where(:promotions_per_shops => { :shop_id => shops_id }).
+         count('id', :distinct => true)
