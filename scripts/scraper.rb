4fdb963bf8a0e40af6b89fdc57215bfa6525d529
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-01 20:48:09 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,12 +1,2 @@
->> [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12].each_slice(4) {|a| p a}
-[1, 2, 3, 4]
-[5, 6, 7, 8]
-[9, 10, 11, 12]
-
-GoogleChart::PieChart.new('320x200', "Things I Like To Eat", false) do |pc| 
-  pc.data "Broccoli", 30
-  pc.data "Pizza", 20
-  pc.data "PB&J", 40 
-  pc.data "Turnips", 10 
-  puts pc.to_url 
-end
+@@variables
+puts
