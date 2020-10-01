6bcccc27b20e76f67b93ac5f825a6aa77faa34f2
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-01 20:48:01 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,2 +1,7 @@
-.inspect
-$:.unshift File.dirname(__FILE__)
+gem 'ruby-oci8', '=1.0.7'
+require 'oci8'           # example is confusing; file required (oci8.rb) is not 
+                         # same name as gem, as is frequently the case
+
+puts object_id
+@a = 'Look, I have instance variables!'
+puts @a
