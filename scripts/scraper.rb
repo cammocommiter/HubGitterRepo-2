14205a9e7343a8833c73f30a0b4f6ab3f3fbf921
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-04 18:27:10 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,2 +1,7 @@
-<<
-open
+#Put this in applictation_controller.rb
+before_filter :log_ram # or use after_filter
+def log_ram
+  logger.warn 'RAM USAGE: ' + `pmap #{Process.pid} | tail -1`[10,40].strip
+end
+
+&
