74b35eef00a522adaae677a7d696300b345cfd59
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-07 12:02:10 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,16 +1,3 @@
-require 'date'
-now = Date.today
-ninety_days_ago = (now - 90)
+gem which rails
 
-namespace :my_tasks do
-  task :foo do
-    do_something
-  end
-
-  task :bar do
-    do_something_else
-  end
-
-end
-
-task :all => ["my_tasks:foo", "my_tasks:bar"]
+hash = Hash.new([])
