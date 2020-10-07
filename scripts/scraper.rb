6159ce0567b8fde4e1fc6a7bc6124b4eb7154181
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-07 13:52:16 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,5 +1,9 @@
-@posts = Post.joins("LEFT OUTER JOIN users ON users.id = posts.user_id").
-              joins(:blog).select
+# Only run the following code when this file is the main file being run
+# instead of having been required or loaded by another file
+if __FILE__==$0
+  # Find the parent directory of this file and add it to the front
+  # of the list of locations to look in when using require
+  $:.unshift File.expand_path("../../", __FILE__)  
+end
 
-10.minutes.ago
-2.days.since
+nil
