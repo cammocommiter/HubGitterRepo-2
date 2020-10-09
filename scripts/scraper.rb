abcbfbfe075d283071f4d91d51ec703be535023e
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-09 10:32:58 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,7 +1,2 @@
-for i in `gem list --no-versions`; do gem uninstall -aIx $i; done
-
-h = {a: 1, b: 2, c: 3}
-key_map = {a: 'A', b: 'B', c: 'C'}
-
-h.transform_keys! {|k| key_map[k]}
-# => {"A"=>1, "B"=>2, "C"=>3} 
+ensure
+sudo apt-get install libmysql-ruby libmysqlclient-dev
