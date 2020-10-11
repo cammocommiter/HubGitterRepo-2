e2ddcf24e876e50d9a995ede3d0d7a1733141a99
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-11 21:48:50 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,2 +1,15 @@
-#each
-result = Users.find(:all, :order => "LOWER(name)")
+\A
+setup do
+  @object = Object.new
+  @object.extend(Greeter)
+end
+
+should "greet person" do
+  @object.stubs(:format).returns("Hello {{NAME}}")
+  assert_equal "Hello World", @object.greet("World")
+end
+
+should "greet person in pirate" do
+  @object.stubs(:format).returns("Avast {{NAME}} lad!")
+  assert_equal "Avast Jim lad!", @object.greet("Jim")
+end
