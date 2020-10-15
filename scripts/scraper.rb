d1543688ff472ca148c004b02e268475308c3376
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-10-15 11:20:07 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,11 +1,16 @@
-class Person < ActiveRecord::Base
-  belongs_to :father, :class_name => 'Person'
-  belongs_to :mother, :class_name => 'Person'
-  has_many :children_of_father, :class_name => 'Person', :foreign_key => 'father_id'
-  has_many :children_of_mother, :class_name => 'Person', :foreign_key => 'mother_id'
-  def children
-     children_of_mother + children_of_father
-  end
+require 'fileutils'
+
+FileUtils.rm_rf('directorypath/name')
+
+class Person
+    attr_accessor :name, :age
+
+    def initialize(name = '', age = 0)
+        self.name = name
+        self.age = age
+    end
 end
 
-HTTParty.get( "#{ @settings.api_server }#{ url }", :verify => false ).parsed_response
+
+Person.new('Ivan', 20)
+Person.new('Ivan')
