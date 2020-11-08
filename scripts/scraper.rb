196b1696e74a9b276ba66ae8a86baff262168b7a
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-11-09 07:17:14 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,2 +1,7 @@
-code
-myclass = Class.new
+require 'time'
+s = "11/23/10 23:29:57"
+Time.parse(s) # => Tue Nov 23 23:29:57 -0800 2010
+s += " UTC"
+Time.parse(s) # => Tue Nov 23 23:29:57 UTC 2010
+
+https://www.example.com:443/path/
