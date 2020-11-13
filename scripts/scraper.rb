4da23bc73b77a59d50f64b4ca3b5d07982e4fe38
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2020-11-14 07:29:28 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,7 +1,2 @@
-require 'time'
-s = "11/23/10 23:29:57"
-Time.parse(s) # => Tue Nov 23 23:29:57 -0800 2010
-s += " UTC"
-Time.parse(s) # => Tue Nov 23 23:29:57 UTC 2010
-
-https://www.example.com:443/path/
+map
+system
