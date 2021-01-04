00945d342c76510e29a4f9bce594b25a797c829a
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2021-01-05 07:06:16 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,2 +1,8 @@
-IN
-phrase2 = phrase1.dup
+before_filter { |c| c.authenticate_rights correct_id_here }
+
+def authenticate_rights(project_id)
+  project = Project.find(project_id)
+  redirect_to signin_path unless project.hidden
+end
+
+rvm use 1.9.3 --default
