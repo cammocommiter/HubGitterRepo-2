21a199416c13b89d04aa2ffb737fd45448502c34
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2021-01-26 22:43:59 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,8 +1,2 @@
-before_filter { |c| c.authenticate_rights correct_id_here }
-
-def authenticate_rights(project_id)
-  project = Project.find(project_id)
-  redirect_to signin_path unless project.hidden
-end
-
-rvm use 1.9.3 --default
+.flatten
+Foo.constants
