d5a72d1a17b96fefd04b9749f9f1a3d382055609
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2021-06-13 12:02:23 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,5 +1,11 @@
-sudo apt-get install vim-nox;
+irb(main):001:0> a = (1..4).to_a
+=> [1, 2, 3, 4]
+irb(main):002:0> a.join("\n")
+=> "1\n2\n3\n4"
+irb(main):003:0> puts a.join("\n")
+1
+2
+3
+4
 
-rescue Exception => e
-  # ...
-end
+puts lines
