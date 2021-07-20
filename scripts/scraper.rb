f2ac6c6539664128bd5a1836b88049b37f281a28
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2021-07-20 17:04:28 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,2 +1,19 @@
-config.yml
-Array#uniq
+describe Array do
+  describe "with 3 items" do
+    before { @arr = [1, 2, 3] }
+
+    specify { @arr.should_not be_empty }
+    specify { @arr.count.should eq(3) }
+  end
+end
+
+describe Array do
+  describe "with 3 items" do
+    subject { [1, 2, 3] }
+
+    it { should_not be_empty }
+    its(:count) { should eq(3) }
+  end
+end
+
+rails_admin
