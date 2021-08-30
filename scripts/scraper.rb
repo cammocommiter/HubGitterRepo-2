19b71824c57d64d7d9c6a7edca83db94a6264aac
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2021-08-30 22:13:01 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,42 +1,5 @@
-..
-module Abstract
-  def abstract_methods(*args)
-    args.each do |name|
-      class_eval(<<-END, __FILE__, __LINE__)
-        def #{name}(*args)
-          raise NotImplementedError.new("You must implement #{name}.")
-        end
-      END
-      # important that this END is capitalized, since it marks the end of <<-END
-    end
-  end
-end
+@search = Model.new_search(params[:search])
+@search.condition.field_starts_with = "prefix"
+@models = @search.all
 
-require 'rubygems'
-require 'rspec'
-
-describe "abstract methods" do
-  before(:each) do
-    @klass = Class.new do
-      extend Abstract
-
-      abstract_methods :foo, :bar
-    end
-  end
-
-  it "raises NoMethodError" do
-    proc {
-      @klass.new.foo
-    }.should raise_error(NoMethodError)
-  end
-
-  it "can be overridden" do
-    subclass = Class.new(@klass) do
-      def foo
-        :overridden
-      end
-    end
-
-    subclass.new.foo.should == :overridden
-  end
-end
+disable-binary
