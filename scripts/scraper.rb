380486a52d9db5c076a76964cd3ccd33ad8b742a
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2021-09-07 17:05:47 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,13 +1,3 @@
-# remove all old versions of the gem
-gem cleanup rjb
+cats.each { |cat| cat.name }
 
-# choose which ones you want to remove
-gem uninstall rjb
-
-# remove version 1.1.9 only
-gem uninstall rjb --version 1.1.9
-
-# remove all versions less than 1.3.4
-gem uninstall rjb --version '<1.3.4'
-
-123
+return hashValue.ToString();
