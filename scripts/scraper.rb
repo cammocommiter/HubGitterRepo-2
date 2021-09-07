3755f4db2b3bd60d4677bbfdcf70fe36f64a1cdd
cammocommiter <71806144+cammocommiter@users.noreply.github.com>
2021-09-07 17:05:46 +0800

    Updating content

--- scripts/scraper.rb
+++ scripts/scraper.rb
@@ -1,5 +1,13 @@
-@search = Model.new_search(params[:search])
-@search.condition.field_starts_with = "prefix"
-@models = @search.all
+# remove all old versions of the gem
+gem cleanup rjb
 
-disable-binary
+# choose which ones you want to remove
+gem uninstall rjb
+
+# remove version 1.1.9 only
+gem uninstall rjb --version 1.1.9
+
+# remove all versions less than 1.3.4
+gem uninstall rjb --version '<1.3.4'
+
+123
